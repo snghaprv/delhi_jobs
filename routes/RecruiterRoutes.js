db92900d33da4debb7258c9c7983ba06dc1e4c2c
Apurva Singh <apurva.singh@naukri.com>
2020-06-19 12:26:31 +0530

    some API end point name changes

--- routes/RecruiterRoutes.js
+++ routes/RecruiterRoutes.js
@@ -11,9 +11,11 @@ cb = (req,res) => {
 RecruiterRouter.use('/',isRecruiterAuthenticated);
 RecruiterRouter.route("/edit-profile").post(editProfile);
 RecruiterRouter.route("/profile").get(getProfile);
-RecruiterRouter.route("/job").post(createJob);
+RecruiterRouter.route("/jobs").post(createJob);
 RecruiterRouter.route("/jobs").get(getAllPostedJobs);
-RecruiterRouter.route("/jobs/:job_id").get(cb);
 RecruiterRouter.route("/jobs/:job_id").patch(editJob);
+RecruiterRouter.route("/jobs/:job_id/applicants").get(cb);
+RecruiterRouter.route("/jobs/:job_id/applicants/:applicant_id").post(cb);
+
 
 module.exports = RecruiterRouter;
