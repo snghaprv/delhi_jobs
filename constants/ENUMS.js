2eb39c1f01dc903f36e5c4d0d459f5435a9d255e
Apurva Singh <apurva.singh@naukri.com>
2020-06-24 21:14:37 +0530

    hindi translation done for ENUMs, categories, localities, qualifications

--- constants/ENUMS.js
+++ constants/ENUMS.js
@@ -1,21 +1,23 @@
+
+// {Model:JobSeeker, Column: gender}
 const JOB_SEEKER_GENDER = [
-    { "id": "MALE", "label": "Male" },
-    { "id": "FEMALE", "label": "Female" }
+    { "id": "MALE", "label": "पुस्र्ष" },
+    { "id": "FEMALE", "label": "महिला" },
+    {"id": "OTHER", "label": "अन्य"}
   ];
-
-
+// {Model:Job, Column: gender}
 const JOB_POST_GENDER = [
     { "id": "ANY", "label": "Any" },
-    { "id": "MALE", "label": "Male" },
-    { "id": "FEMALE", "label": "Female" }
+    { "id": "MALE", "label": "पुस्र्ष" },
+    { "id": "FEMALE", "label": "महिला" }
   ]
-
+// {Model:Job, Column: working_days}
 const WORKING_DAYS  =  [
     { "id": "5_DAYS_WORKING", "label": "5 days working" },
     { "id": "6_DAYS_WORKING", "label": "6 days working" },
     { "id": "OTHERS", "label": "Others" }
   ]
-
+// {Model:Job, Column: job_type}
 const JOB_TYPE = [
     { "id": "FULL_TIME", "label": "Full time" },
     { "id": "PART_TIME", "label": "Part time" },
