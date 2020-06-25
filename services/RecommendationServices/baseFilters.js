ba2d25f3c7404248fa2b0fc235c5f1e089734a42
Apurva Singh <apurva.singh@naukri.com>
2020-06-25 20:21:35 +0530

    title changes

--- services/RecommendationServices/baseFilters.js
+++ services/RecommendationServices/baseFilters.js
@@ -3,15 +3,15 @@ const {localities,categories} = metadata;
 
 const getFilters = async function(){
     const filters = [{
-        "header": "Locality",
-        "title" : "Locality",
+        "header": "जिला चुनें (District)",
+        "title" : "जिला चुनें (District)",
         "id": "locality",
         "type": "multi_select",
         "is_selected": false,
         "options": localities.map(({id,label}) => ({id,label,is_selected:false}))
     },{
-        "header": "Categories",
-        "title" : "Categories",
+        "header": "रोल चुनें (Job Category)",
+        "title" : "रोल चुनें (Job Category)",
         "id": "category",
         "type": "multi_select",
         "is_selected": false,
