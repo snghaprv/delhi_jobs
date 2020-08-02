53aafa1050a7a3fd96605cad3490196f6b079bda
ravi18 <ravi@arrowai.com>
2020-08-02 21:58:33 +0530

    changes in app.js for cluster

--- app.js
+++ app.js
@@ -1,50 +1,65 @@
-require("dotenv").config();
-//const {GENERIC_ERROR} = require("./constants/ErrorMessageConstants");
-const express = require("express");
+var cluster = require('cluster');
 
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const fs = require("fs");
-const path = require("path");
-const cors = require("cors");
-const rfs = require("rotating-file-stream");
+if (cluster.isMaster) {
+    // Leaving one core for the frontend UI app
+    var cpuCount = require('os').cpus().length - 1;
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-const port = process.env.PORT || 3000;
-const db = require("./database/models");
-const {
-  AuthRoutes,
-  JobSeekerRoutes,
-  MetaDataRoutes,
-  RecruiterRoutes,
-} = require("./routes");
-const responseFormatter = require("./middleware/responseFormatter");
-// const accessLogStream = rfs('access.log', {
-//     interval: '1d', // rotate daily
-//     path: path.join(__dirname, 'access_log')
-//   })
-// app.use(morgan('combined', {stream :accessLogStream }));
-app.use(responseFormatter);
-app.get("/", (req, res) => {
-  res.json({ message: `Delhi Government is here is Help You ..!!` });
-});
-app.use("/metadata", MetaDataRoutes);
-app.use("/login", AuthRoutes);
-app.use("/job-seeker", JobSeekerRoutes);
-app.use("/recruiter", RecruiterRoutes);
-
-app.get("*", (req, res) => {
-  res.status(404).json({ error: `Couldn't find the route.` });
-});
-
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(400).json({ error: `Something Bad Happened.` });
-});
-
-app.listen(port, () => {
-  console.log(`Running on ${port}`);
-});
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+    cluster.on('exit', function (worker) {
+        console.log('Replacing dead Worker %d', worker.id);
+        cluster.fork();
+    });
+} else {
+  require("dotenv").config();
+  //const {GENERIC_ERROR} = require("./constants/ErrorMessageConstants");
+  const express = require("express");
+  
+  const bodyParser = require("body-parser");
+  const morgan = require("morgan");
+  const fs = require("fs");
+  const path = require("path");
+  const cors = require("cors");
+  const rfs = require("rotating-file-stream");
+  
+  const app = express();
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  const port = process.env.PORT || 3000;
+  const db = require("./database/models");
+  const {
+    AuthRoutes,
+    JobSeekerRoutes,
+    MetaDataRoutes,
+    RecruiterRoutes,
+  } = require("./routes");
+  const responseFormatter = require("./middleware/responseFormatter");
+  // const accessLogStream = rfs('access.log', {
+  //     interval: '1d', // rotate daily
+  //     path: path.join(__dirname, 'access_log')
+  //   })
+  // app.use(morgan('combined', {stream :accessLogStream }));
+  app.use(responseFormatter);
+  app.get("/", (req, res) => {
+    res.json({ message: `Delhi Government is here is Help You ..!!` });
+  });
+  app.use("/metadata", MetaDataRoutes);
+  app.use("/login", AuthRoutes);
+  app.use("/job-seeker", JobSeekerRoutes);
+  app.use("/recruiter", RecruiterRoutes);
+  
+  app.get("*", (req, res) => {
+    res.status(404).json({ error: `Couldn't find the route.` });
+  });
+  
+  app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(400).json({ error: `Something Bad Happened.` });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Running on ${port}`);
+  });
+}
