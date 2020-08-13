86971da93a0f48c448bea7fd245d002e4dbf0b00
ravi18 <ravi@arrowai.com>
2020-08-13 22:58:40 +0530

    aoxis changed to request

--- services/SMS.js
+++ services/SMS.js
@@ -1,5 +1,18 @@
 const axios = require("axios");
 const https = require("https");
+const request =require("request");
+const doRequest=(url)=> {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, res, body) {
+      if (!error && res.statusCode == 200) {
+        resolve(body);
+      } else {
+        console.log(error);
+        reject(error);
+      }
+    });
+  });
+}
 
 const sendSMSOnProduction = async function (content, phone) {
   let {
@@ -8,15 +21,17 @@ const sendSMSOnProduction = async function (content, phone) {
     GOVT_SMS_SIGNATURE: signature,
   } = process.env;
   let url = `https://smsgw.sms.gov.in/failsafe/HttpLink?username=${username}&pin=${pin}&message=${content}&mnumber=${phone}&signature=${signature}`;
-  const axios_instance = axios.create({
-    httpsAgent: new https.Agent({
-      rejectUnauthorized: false,
-    }),
-  });
+  // const axios_instance = axios.create({
+  //   httpsAgent: new https.Agent({
+  //     rejectUnauthorized: false,
+  //   }),
+  // });
 
-  let response = await axios_instance.get(url);
+  // let response = await axios_instance.get(url);
+  let response=  await doRequest(url);
+
   if (response.status != 200) {
-    console.error(response.data);
+    console.error(response);
     throw "OTP_SENDING_FAILURE";
   }
 
@@ -29,18 +44,18 @@ const sendSMSOnTesting = async function (content, phone, message_type) {
     PROMOTIONAL: process.env.KALEYRA_PROMOTIONAL_API_KEY,
   };
   let url = `https://api-global.kaleyra.com/v4/?method=sms&api_key=${KALEYRA_KEYS[message_type]}&to=91${phone}&message=${content}&format=1122334455667788991010___XXXXXXXXXX&sender=${process.env.KALEYRA_SENDER_ID}`;
-
-  let response = await axios.get(url);
-  if (response.data.status != "OK") {
-    console.error(response.data);
+  // let response = await axios.get(url);
+  let response= await doRequest(url);
+  console.log(response);
+  if (response.status != "OK") {
+    console.error(response);
     throw "OTP_SENDING_FAILURE";
   }
   return response;
 };
-
 const sendMessage = async function (content, phone, message_type) {
   let response =
-    process.env.NODE_ENV == "production"
+    process.env.NODE_ENV == "development"
       ? await sendSMSOnProduction(content, phone, message_type)
       : await sendSMSOnTesting(content, phone, message_type);
   return response;
